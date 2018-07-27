563d8ea30bc444649143b46145323ca736be6154
Sharath Kumar K J <kjsharath3@gmail.com>
2018-07-28 01:00:46 +0530

    adedd

--- IQVIA/Classes/Swagger.cs
+++ IQVIA/Classes/Swagger.cs
@@ -4,6 +4,7 @@ namespace IQVIA.Controllers
 {
     public class Swagger : ISwagger
     {
+
         public string Id { get; set; }
         public string Stamp { get; set; }
         public string Text { get; set; }
